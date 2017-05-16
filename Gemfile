385f0d40c3e475852b69f1fbc0c5b97a251363f4
victor hazbun <victorhazbun87@gmail.com>
2017-05-15 19:27:20 -0500

    Ruby Design Patterns In Practice

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in ruby_design_patterns_in_practice.gemspec
+gemspec
---
+++ lib/ruby_design_patterns_in_practice.rb
@@ -0,0 +1,5 @@
+require "ruby_design_patterns_in_practice/version"
+
+module RubyDesignPatternsInPractice
+  # Your code goes here...
+end
